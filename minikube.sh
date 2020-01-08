c8ff12c9a451d49e9c5145be7cc642b0e0b8327e
on0t0le <bugrovka12@ukr.net>
2020-01-08 09:35:59 +0200

    Updated shell script

--- minikube.sh
+++ minikube.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-minikube start --kubernetes-version v1.14.9 --cpus=2 --memory='4g' --extra-config=apiserver.authorization-mode=RBAC
+minikube start --kubernetes-version v1.16.4 --cpus=2 --memory='4g' --extra-config=apiserver.authorization-mode=RBAC
